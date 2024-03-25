00c863178671dc2c6e998723f9ab168bb65c8f5a
Reshula <reyhana328@gmail.com>
2024-03-25 17:05:45 -0400

    Loader Component added,modified in NavBar.css

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
    
   <NavBar />
   <Routes>
-    <Route exat path='/home' element={<Home/>}/>
+    <Route exat path='/' element={<Home/>}/>
     <Route path='/shop' element={<Shop/>}/> 
     <Route path='/about' element={<About/>}/>
     <Route path='/contact' element={<Contact/>}/> 
--- src/Store/Shop.js
+++ src/Store/Shop.js
@@ -4,22 +4,22 @@ import Jewelery from "./Jewelery";
 import Button from "./Button";
 import './Store.css'
 
-// import  LoaderPage  from './Loader/LoaderPage';
+import LoaderPage from "../Loader/LoaderPage";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 
 function Shop(){
     const [jewelery, setJewelery] = useState(data);
-    // const [stateLoader, setStateLoader] = useState(false);
+    const [stateLoader, setStateLoader] = useState(false);
 
   
     
-// useEffect(() =>{
-//     const timer = setTimeout (() =>setStateLoader(false), 3000);
-//     return() => clearTimeout(timer)
+    useEffect(() =>{
+    const timer = setTimeout (() =>setStateLoader(false), 3000);
+    return() => clearTimeout(timer)
     
-//     }, [])
+    }, [])
     useEffect(() => {
         AOS.init();
       }, [])
@@ -35,7 +35,7 @@ function Shop(){
 
     return(
         <div className="containerShop">
-            {/* {stateLoader && <LoaderPage/>} */}
+            {stateLoader && <LoaderPage/>}
             <h1  data-aos="flip-right" className="titleShop">Shop by category</h1>
             <Button filteredJew={ choosenJew}/>
             <Jewelery jewelery= {jewelery}/>
